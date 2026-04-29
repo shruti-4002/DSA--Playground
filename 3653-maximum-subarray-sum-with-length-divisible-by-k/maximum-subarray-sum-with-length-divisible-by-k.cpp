205bb79ba1c896bf00e3c0301fc824537cf608d8
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-04-29 22:45:10 +0530

    Time: 106 ms (10.22%) | Memory: 184.8 MB (10.70%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -2,25 +2,36 @@ class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
 
-        vector<long long> mp(k, LLONG_MAX);
-        mp[0] = 0;
+        unordered_map<int,long long>mp;
+        mp[0]=0;
 
-        long long prefix = 0;
-        long long ans = LLONG_MIN;
+        long long currsumtillnow=0;
+        long long ans=LLONG_MIN;
+        long long leastprefixsum=0;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            currsumtillnow+=nums[i];
+            int rem=(i+1)%k;
 
-        for(int i = 0; i < nums.size(); i++) {
+            if(mp.find(rem)==mp.end()){
+                mp[rem]=currsumtillnow;
+                continue;
+            }
+            
+           
+           long long  sum=currsumtillnow-mp[rem];
 
-            prefix += nums[i];
-
-            int rem = (i + 1) % k;
-
-            if(mp[rem] != LLONG_MAX) {
-                ans = max(ans, prefix - mp[rem]);
+            if(sum>ans){
+                ans=sum;
             }
 
-            mp[rem] = min(mp[rem], prefix);
+            leastprefixsum= min(mp[rem],currsumtillnow);
+            mp[rem]=leastprefixsum;
+
         }
 
         return ans;
+
     }
 };
