49a44ea7da52d1127906a8549219c64a2a5ade35
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-05-02 11:16:18 +0530

    Time: 71 ms (94.36%) | Memory: 146.4 MB (52.70%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        int Sj=0;
-        int length=0;
+        unordered_map<int ,int>mp;
         mp[0]=-1;
-
+        int curr=0;
         for(int i=0;i<nums.size();i++){
 
-            Sj+=nums[i];
+            curr+=nums[i];
+            int rem=curr%k;
 
-            int rem=Sj%k;
-            
-
-//agarsame rem exits akrta hai toh conform hai ki mutiple of K HAI UNKE BITCH KA  SUBARRAY KA SUM 
-//BUS SUM MUTIPLE HAIN YA NAHI NO NEED TOC HECK JUST CHECK GAP
             if(mp.find(rem)!=mp.end()){
-            
-            length=i-mp[rem];
-            if(length>=2)
-            return true;
-    
-
+                int len=i-mp[rem];
+                if(len>=2)return true;
             }else{
-                      mp[rem]=i;   
-
+                mp[rem]=i;
+                
             }
-
-          
-        }
+        }   
 
         return false;
     }
