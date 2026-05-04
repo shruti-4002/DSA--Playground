ed59da32ffaef36b8188473be919931f426cd6a8
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-05-04 13:16:25 +0530

    Time: 4 ms (20.61%) | Memory: 42.2 MB (24.60%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -11,18 +11,16 @@ public:
 
         for(int i=1;i<nums.size();i++){
             pre[i]=pre[i-1]*nums[i-1];
-            // ans[i]*=pre[i];
+            ans[i]*=pre[i];
 
         }
 
         for(int i=n-2;i>=0;i--){
             suf[i]=suf[i+1]*nums[i+1];
-            // ans[i]*=suf[i];
+            ans[i]*=suf[i];
         }
 
-        for(int i=0;i<nums.size();i++){
-            ans[i]=pre[i]*suf[i];
-        }
+       
         return ans;
     }
 };
