53d2c6d674a6dd3492eaffd7b38a3ed7b5c640a9
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-05-15 21:50:02 +0530

    Time: 8 ms (34.49%) | Memory: 19.9 MB (39.94%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,71 +1,40 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
-        vector<vector<int>>adj(numCourses);//adjaceny list
-        vector<int>ans;
-        vector<int>indegree(numCourses,0);//incoming edges
+        unordered_map<int,vector<int>>mp;
+        vector<int>indegree(numCourses,0);
         queue<int>q;
 
-
-        for(auto& p : prerequisites){
-            int a=p[0];
-            int b=p[1];
-            //[]-->[]; children
-            adj[b].push_back(a);
-            indegree[a]++;
-            
+        for(auto & p : prerequisites){
+           int child= p[0];
+           int parent=p[1];
+           mp[parent].push_back(child);
+            indegree[child]++;
         }
-
-        //check whose indegree is 0 remebr in topo sort we add 
-        //elements with indegree 0 only to queue;
-        //keep reducing the indgree until it becomes 0;
-
 
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.push(i);
             }
+
         }
 
+        int count=0;
+
         while(!q.empty()){
-
-            auto curr = q.front();
-            ans.push_back(curr);
+            int curr=q.front();
             q.pop();
-
-            for(auto x: adj[curr]){
-
+            count++;
+            for(auto x : mp[curr]){
                 indegree[x]--;
-
                 if(indegree[x]==0){
                     q.push(x);
                 }
-
-
             }
-            
-
-
-
-
-
         }
 
-        if(ans.size()!=numCourses){
-            return false;
-        }
+        if(count==numCourses)return true;
 
-        return true;
-
-
-
-
-
-
-
-
-
-
+        return false;
     }
 };
