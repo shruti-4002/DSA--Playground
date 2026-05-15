bc860bbf1646f72cc9bb4b4d04487fb6c727f042
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-05-15 21:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (39.21%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(numCourses);
+        
+
+        unordered_map<int,vector<int>>mp;
         vector<int>indegree(numCourses,0);
         vector<int>ans;
-
         queue<int>q;
 
-        for(auto &p: prerequisites){
-            int child=p[0];
-            int parent=p[1];
-
-            adj[parent].push_back(child);
-
+        for(auto & p : prerequisites){
+           int child= p[0];
+           int parent=p[1];
+           mp[parent].push_back(child);
             indegree[child]++;
-
         }
-
 
         for(int i=0;i<numCourses;i++){
             if(indegree[i]==0){
                 q.push(i);
             }
+
         }
 
+        int count=0;
 
         while(!q.empty()){
             int curr=q.front();
             ans.push_back(curr);
             q.pop();
-            for(auto x: adj[curr]){
+            count++;
+            for(auto x : mp[curr]){
                 indegree[x]--;
                 if(indegree[x]==0){
                     q.push(x);
                 }
             }
-
         }
 
-        return ans.size()==numCourses?ans:vector<int>{};
-
-
-
+        if(count==numCourses)return ans;
+        return {};
+       
+   
     }
 };
