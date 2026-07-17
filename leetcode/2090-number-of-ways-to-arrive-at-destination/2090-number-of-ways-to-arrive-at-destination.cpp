1fb083b7bf29c6686187c0b00f8413477fb91adf
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-07-17 12:48:15 +0530

    Time: 11 ms (45.22%), Space: 37.4 MB (32.85%) - CodeHub

--- leetcode/2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ leetcode/2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,59 +1,50 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-       
         priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+            long long mod=1e9+7;
+        vector<vector<pair<int,int>>>adj(n);
         
-           long long mod=1e9+7;
-        
-      
-        vector<vector<pair<int, int>>> adj(n); 
-        vector<long long> ways(n, 0); 
-       
-        for(auto& e : roads){ 
-            int u = e[0];
-            int v = e[1];
-            int w = e[2];
+        for(auto e:roads){
+            int u=e[0];
+            int v=e[1];
+            int w=e[2];
             
-            adj[u].push_back({v, w});
-            adj[v].push_back({u, w});
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
         
-       
-        vector<long long> dis(n, LLONG_MAX); 
         
-       
-        dis[0] = 0;
-        ways[0] = 1; 
-        pq.push({dis[0], 0});
+        vector<long long>dis(n, LLONG_MAX);
+        vector<long long>ways(n,0);
+        dis[0]=0;
+        ways[0]=1;
+        pq.push({dis[0],0});
         
         while(!pq.empty()){
-            long long DisNodeA = pq.top().first;
-            int NodeA = pq.top().second;
-            pq.pop();
             
-         
-         
-            if (DisNodeA > dis[NodeA]) continue;
+                long long DisNodeA=pq.top().first;
+                int NodeA=pq.top().second;
                 
-            for(auto& nodes : adj[NodeA]){
-                int NodeB = nodes.first;
-                long long EdgeDisNodeB = nodes.second;
+                pq.pop();
                 
-               
-                if(dis[NodeB] > DisNodeA + EdgeDisNodeB){
-                    dis[NodeB] = DisNodeA + EdgeDisNodeB;
-                    ways[NodeB]=ways[NodeA];
-                    pq.push({dis[NodeB], NodeB});
+                if (DisNodeA > dis[NodeA]) continue;
+                
+                for(auto & nodes : adj[NodeA]){
+                    int NodeB=nodes.first;
+                    long long EdgeDisNodeB=nodes.second;
+                    if(dis[NodeB]>DisNodeA+EdgeDisNodeB){
+                        ways[NodeB]=ways[NodeA];
+                        dis[NodeB]=DisNodeA+EdgeDisNodeB;
+                        pq.push({dis[NodeB],NodeB});
+                    }
+                    else if(dis[NodeB]==DisNodeA+EdgeDisNodeB){
+                        ways[NodeB]=(ways[NodeA]+ways[NodeB])%mod;
+                    }
                 }
-              
-                else if(dis[NodeB] == DisNodeA + EdgeDisNodeB){
-                    ways[NodeB]=(ways[NodeA]+ways[NodeB])%mod;
-                }
-            }
+            
         }
         
-       
-        return ways[n - 1]; 
+        return ways[n-1];
     }
 };
