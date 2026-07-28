6f26b6863ff8621ec6ce781f0a5f03c95a7128de
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-07-29 00:47:58 +0530

    Time: 15 ms (92.73%), Space: 17.8 MB (84.63%) - CodeHub

--- leetcode/0322-coin-change/0322-coin-change.cpp
+++ leetcode/0322-coin-change/0322-coin-change.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-         vector<int>dp(amount+1,1e9);
+         vector<unsigned int>dp(amount+1,1e9);
         
         dp[0]=0;
         for(int i=0;i<coins.size();i++){
