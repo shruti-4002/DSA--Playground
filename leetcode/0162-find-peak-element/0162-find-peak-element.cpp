cd55ba01934748cbc3e32cf7ce70a55a0a5d1635
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-08-15 20:07:13 +0530

    Time: 0 ms (100%), Space: 12.8 MB (7.41%) - CodeHub

--- leetcode/0162-find-peak-element/0162-find-peak-element.cpp
+++ leetcode/0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
-        int start=0;
+       int start=0;
         int end=arr.size()-1;
         while(start<end){
-            int mid=((end-start)/2)+start;
-
-    
+            int mid=start+((end-start)/2);
             if(arr[mid]<arr[mid+1]){
                 start=mid+1;
-                
             }else{
                 end=mid;
             }
-
         }
 
         return start;
