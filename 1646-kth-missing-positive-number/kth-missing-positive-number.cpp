bb5b46e751b35e12b6f6360964244e09accc72c6
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-08-15 15:08:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (93.21%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -8,13 +8,14 @@ public:
             int missing = arr[mid] - (mid + 1);
 
             if (missing < k)
+                
                 low = mid + 1;
             else
                 high = mid - 1;
         }
     
 
-        return low+k;
+        return high+1+k;
 
     }
 };
