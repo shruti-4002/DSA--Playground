aff7521336d02c953c9f68587b11f04f66c68b2b
Shruti Singh <151828483+shruti-4002@users.noreply.github.com>
2026-08-16 01:47:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (96.87%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int moneyleft=n;
-        int count=0;
-        for(int i=1;i<=n;i++){
-             moneyleft=moneyleft-i;
-             if(moneyleft>=0){
-                count++;
-             }else{
-                break;
-             }
+        long long start=1;
+        long long end=n;
+        
+        
+        while(start<=end){
+            long long mid=(end-start)/2+start;
+            long long stm=(mid*(mid+1))/2;
+
+        
+            if(stm>n){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
         }
-        return count;
+
+        return end;
     }
 };
